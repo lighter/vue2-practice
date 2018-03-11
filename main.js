c9611bb041b632c259d86751a4853538c3dee252
lighter <apple.xva@gmail.com>
2018-03-11 17:10:37 +0800

    Components Within Components

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+Vue.component('task-list', {
+    template: `
+     <div><task v-for="task in tasks">{{ task.task }}</task></div>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'AAAAA', complete: true },
+                { task: 'BBBBB', complete: true },
+                { task: 'CCCCC', complete: true },
+                { task: 'DDDDD', complete: true },
+            ]
+        };
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
